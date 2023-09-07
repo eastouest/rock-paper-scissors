c2e7eb323dcdd8731d0e1af29ee6ef5643120ec6
Amelie Sutton <112052610+eastouest@users.noreply.github.com>
2023-09-06 20:54:58 -0700

    Write function for computer choice

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    var choice = Math.random() * 3;
+    if (choice <= 1) {
+        choice = "Rock";
+    } else if (choice <= 2) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    };
+    return choice;
+}
