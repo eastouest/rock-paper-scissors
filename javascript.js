00583dc7dc55298189e780795b545d24eaf96aed
Amelie Sutton <112052610+eastouest@users.noreply.github.com>
2023-09-06 21:20:03 -0700

    Write function to play a round

--- javascript.js
+++ javascript.js
@@ -12,3 +12,37 @@ function getComputerChoice() {
 
 let playerSelection = prompt("Please type your choice: Rock, paper, scissors")
 alert("You chose " + playerSelection)
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "You draw. Try again"
+    }
+    else if (playerSelection.toLowerCase() == "rock") {
+        if (computerSelection == "Paper") {
+            return "You lose. Refresh to play again."
+        }
+        else if (computerSelection == "Scissors") {
+            return "You win. Refresh to play again."
+        }
+    }
+    else if (playerSelection.toLowerCase() == "scissors") {
+        if (computerSelection == "Paper") {
+            return "You win. Refresh to play again."
+        }
+        else if (computerSelection == "Rock") {
+            return "You lose. Refresh to play again."
+        }
+        else if (playerSelection.toLowerCase() == "paper") {
+            if (computerSelection == "Rock") {
+                return "You win. Refresh to play again."
+            }
+            else if (computerSelection == "Scissors") {
+                return "You lose. Refresh to play again."
+            }
+        }
+    }
+};
+
+computerSelection = getComputerChoice();
+playerSelection = "rock";
+playRound(playerSelection, computerSelection)
