e7743228f7153e442fbd72c3ad40cbd68511076b
Amelie Sutton <112052610+eastouest@users.noreply.github.com>
2023-12-22 16:04:19 -0800

    Refactor game function and place it in event listener

--- javascript.js
+++ javascript.js
@@ -10,27 +10,9 @@ function getComputerChoice() {
     };
     return choice;
 }
-
+let max_points = 5;
 let computerWinCounter = 0;
 let playerWinCounter = 0;
-
-let result = document.createElement('div');
-result.textContent = "The result will be displayed here.";
-let result_field = document.createElement('div');
-container.style = "width: 100%; height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: 20px;";
-result_field.style = "border: 1px solid black; width: 300px; height: 100px; margin: 0 auto; text-align: center; padding-top: 40px; font-size: 20px; display: block;";
-container.appendChild(result_field);
-result_field.appendChild(result);
-
-let buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        playerSelection = button.id;
-        computerSelection = getComputerChoice();
-        alert(playRound(playerSelection, computerSelection));
-    });
-});
 
 //define the game of Rock, Paper, Scissors and return a greeting
 function playRound(playerSelection, computerSelection) {
@@ -70,61 +52,36 @@ function playRound(playerSelection, computerSelection) {
     }
 };
 
-function game() {
-    max_points = 5;
-    while (computerWinCounter < max_points && playerWinCounter < max_points) {
-        // alert("Hello!")
-    }
-        if (computerWinCounter == max_points || playerWinCounter == max_points) {
-            if (computerWinCounter > playerWinCounter) {
-                alert("You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You lose. Refresh to play again.")
-            }
-            else if (playerWinCounter > computerWinCounter) {
-                alert("You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You win. Refresh to play again.")
-            }
-            else {
-                alert("You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You draw. Refresh to play again.")
-            }
+function checkGameEnd() {
+    if (computerWinCounter == max_points || playerWinCounter == max_points) {
+        if (computerWinCounter > playerWinCounter) {
+            alert("You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You lose. Refresh to play again.");
+            result.textContent = "You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You lose. Refresh to play again.";
         }
-    // let playerSelection = prompt("Please type your choice: Rock, paper, scissors");
-    // computerSelection = getComputerChoice();
-    // console.log(computerSelection);
-    // alert(playRound(playerSelection, computerSelection));
-    // //round 1
-    // let playerSelection = prompt("Please type your choice: Rock, paper, scissors");
-    // computerSelection = getComputerChoice();
-    // console.log(computerSelection);
-    // alert(playRound(playerSelection, computerSelection));
-    // //round 2
-    // playerSelection = prompt("Please type your choice: Rock, paper, scissors");
-    // computerSelection = getComputerChoice();
-    // console.log(computerSelection);
-    // alert(playRound(playerSelection, computerSelection));
-    // //round 3
-    // playerSelection = prompt("Please type your choice: Rock, paper, scissors");
-    // computerSelection = getComputerChoice();
-    // console.log(computerSelection);
-    // alert(playRound(playerSelection, computerSelection));
-    // //round 4
-    // playerSelection = prompt("Please type your choice: Rock, paper, scissors");
-    // computerSelection = getComputerChoice();
-    // console.log(computerSelection);
-    // alert(playRound(playerSelection, computerSelection));
-    // //round 5
-    // playerSelection = prompt("Please type your choice: Rock, paper, scissors");
-    // computerSelection = getComputerChoice();
-    // console.log(computerSelection);
-    // alert(playRound(playerSelection, computerSelection));
-
-    // if (computerWinCounter > playerWinCounter) {
-    //     alert("You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You lose. Refresh to play again.")
-    // }
-    // else if (playerWinCounter > computerWinCounter) {
-    //     alert("You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You win. Refresh to play again.")
-    // }
-    // else {
-    //     alert("You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You draw. Refresh to play again.")
-    // }
+        else if (playerWinCounter > computerWinCounter) {
+            alert("You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You win. Refresh to play again.");
+            result.textContent = "You have " + playerWinCounter + " points. The computer has " + computerWinCounter + " points. You win. Refresh to play again.";
+        }
+    }
 }
 
-// game();
+let result = document.createElement('div');
+result.textContent = "The result will be displayed here.";
+let result_field = document.createElement('div');
+container.style = "width: 100%; height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: 20px;";
+result_field.style = "border: 1px solid black; width: 300px; height: 100px; margin: 0 auto; text-align: center; padding-top: 40px; font-size: 20px; display: block;";
+container.appendChild(result_field);
+result_field.appendChild(result);
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = getComputerChoice();
+        alert(playRound(playerSelection, computerSelection));
+        result.textContent = "Computer has " + computerWinCounter + " points. You have " + playerWinCounter + " points.";
+        checkGameEnd();
+    });
+});
+
